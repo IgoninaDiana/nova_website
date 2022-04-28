46119d8aedddcf143d6e5a8daa3124b392dca3b4
Gerasimov Andrey <andrey.gerasimov2506@gmail.com>
2022-04-28 12:26:43 +0300

    Gerasimov Andrey

--- m/lk/content.php
+++ m/lk/content.php
@@ -3,13 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Личный кабинет</title>
+        <title><?php if () : ?><?php else : ?><?php endif ?></title>
         <link rel="stylesheet" href="../css/style.css">
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.4.4/mobile-detect.min.js"></script>
         <script>
             let detect = new MobileDetect(window.navigator.userAgent);
             if (detect.mobile() == null) {
+                var get = '<?php if (isset($_GET["id"])) {echo "?id=".$_GET["id"];}; if (isset($_GET["rubrika"])) {echo "?rubrika=".$_GET["rubrika"];}; ?>';
                 $(location).attr('href', '/lk');
             }
         </script>
